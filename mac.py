d8c2503240de220af55c6568b5d99c0ffb36ba1e
root <root@localhost.localdomain>
2021-07-26 13:17:34 -0400

    first commit

---
+++ mac.py
@@ -0,0 +1,210 @@
+#
+# =======================================+SCRIPT BY Twinkle Hacker+==============================================
+#
+# ========================================================================
+#
+# ================================+contact : www.facebook.com/twinkle=====================================
+#
+# =========================================================================
+#
+# Code written by Twinkle Hacker , 
+#=============================================================================================================
+import sys
+import os
+import time
+print('''
+XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+XX   MMMMMy''                                                    ''yMMMMM   XX
+XX   MMMy'                                                          'yMMM   XX
+XX   Mh'                                                              'hM   XX
+XX   -                                                                  -   XX
+XX                                                                          XX
+XX   ::                                                                ::   XX
+XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+XX                                oo      oo                                XX
+XX           oM                 oo          oo                 Mo           XX
+XX         oMMo                M              M                oMMo         XX
+XX       +MMMM                 s              s                 MMMM+       XX
+XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(10. / 100)
+slowprint("[!] Starting : ")
+time.sleep(5)
+os.system('clear')
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(4. / 100)
+slowprint("    \033[91mThe Easy")
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0. / 100)
+slowprint('''\033[1;31m \033[91m    
+       __  ___                 ________                               
+      /  |/  /___ ______      / ____/ /_  ____ _____  ____ ____  _____
+     / /|_/ / __ `/ ___/_____/ /   / __ \/ __ `/ __ \/ __ `/ _ \/ ___/
+    / /  / / /_/ / /__/_____/ /___/ / / / /_/ / / / / /_/ /  __/ /    
+   /_/  /_/\__,_/\___/      \____/_/ /_/\__,_/_/ /_/\__, /\___/_/     
+                                                   /____/\033[97m             
+''')
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(3. / 100)
+slowprint("\t\t                                         \033[93mBy :Twinkle Hacker\033[97m")
+print(" ")
+print("1- show the current MAC-ADDRESS")
+print("")
+print("2- change your MAC-ADDRESS Randomly")
+print("")
+print("3- change your MAC-ADDRESS Customly")
+print("")
+print("4- reset the original MAC-ADDRESS")
+print(" ")
+print("")
+x=input("\033[92m[?] \033[96mmake your choise ==>")
+if x==('1') :
+            print(" ")
+            print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+            print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+            print(" ")
+            lawla=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+            if lawla==('1') :
+                      slowprint("\033[97m")
+                      os.system('macchanger -s wlan0')
+                      print(" ")
+                      alla=input('press any key to continue')
+                      os.system('clear') 
+                      os.system('python3 mac.py')
+            if lawla==('2') :
+                       slowprint("\033[97m")
+                       os.system('macchanger -s eth0')
+                       print(" ")
+                       allah=input('press any key to continue')
+                       os.system('clear')
+                       os.system('python3 mac.py')
+
+if x==('2') :
+               print(" ")
+               print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+               print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+               print(" ")
+               deuxs=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+               if deuxs==('2') :
+                        slowprint("")
+                        os.system('ifconfig eth0 down')
+                        os.system('macchanger -r eth0')
+                        os.system('ifconfig eth0 up')
+                        print(" ")
+                        hoho=input("press any key to continue")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+               if deuxs==('1') :
+                        slowprint("")
+                        os.system('ifconfig wlan0 down')
+                        os.system('macchanger -r wlan0')
+                        os.system('ifconfig wlan0 up')
+                        print(" ")
+                        hoho=input("press any key to continue")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+
+if x==('4'): 
+               print(" ")
+               print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+               print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+               print(" ")
+               talta=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+               if talta==('2') :
+                        print(" ")
+                        slowprint("\033[97m")
+                        os.system('macchanger -p eth0')
+                        print(" ") 
+                        lopa=input("press any key to continue ")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+               if talta==('1') : 
+                        print(" ")
+                        slowprint("\033[97m")
+                        os.system('ifconfig wlan0 down')
+                        os.system('macchanger -p wlan0')
+                        os.system('ifconfig wlan0 up')
+                        print(" ") 
+                        lopa=input("press any key to continue ")
+                        os.system('clear')
+                        os.system('python3 mac.py')
+
+
+def slowprint(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+
+
+if x==('3'):
+               print(" ")
+               print("\033[94m1 \033[97m- \033[91mwlan0 \033[97m( WIFI connexion)")
+               print("\033[94m2 \033[97m- \033[91meth0 \033[97m( CABLE connection)")
+               print(" ")
+               rabaa=input("   \033[95m[?] \033[97menter your \033[91mconnection type \033[97m:")
+               if rabaa==('2') :
+                        print(" ")
+                        os.system('ifconfig eth0 down')
+                        dire=input("\033[95m[?] \033[97menter THE NEW \033[92mMAC-ADDRESS \033[97m: ")
+                        os.system('ifconfig eth0 down')
+                        os.system('macchanger -m'+(dire)+' eth0')
+                        os.system('ifconfig eth0 up')
+                        print("done")
+               if rabaa==('1') : 
+                        os.system('ifconfig wlan0 down')
+                        print(" ")
+                        dire=input("\033[95m[?] \033[97menter THE NEW \033[92mMAC-ADDRESS \033[97m: ")
+                        os.system('ifconfig wlan0 down')
+                        os.system('macchanger -m'+(dire)+' wlan0')
+                        os.system('ifconfig wlan0 up')
+                        print("done")
+
+
+
+
